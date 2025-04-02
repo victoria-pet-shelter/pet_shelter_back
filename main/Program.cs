8b0aa3e2e65329502258242083e22da9213ff32a
Alan Arzumanjan <alanarzumanjan@gmail.com>
2025-04-02 13:21:13 +0300

    Backend addition

---
+++ main/Program.cs
@@ -0,0 +1,18 @@
+﻿using ORM = Microsoft.EntityFrameworkCore;
+
+namespace Pet_Shelter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            RunApplication();
+        }
+
+        static void RunApplication()
+        {
+            Console.WriteLine("Hello, Pet Shelter!");
+        }
+    }
+}
