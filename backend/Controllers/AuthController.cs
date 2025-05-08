94c8ffa7084c656376f1c123d622726bbd97128d
Alan Arzumanjan <alanarzumanjan@gmail.com>
2025-05-08 14:18:44 +0300

    Added Data Transfer folder and Objects (DTO) and rewrite login/register validation for more professional style

--- backend/Controllers/AuthController.cs
+++ backend/Controllers/AuthController.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using Models;
 using Config;
 using Microsoft.Extensions.Configuration;
+using Dtos;
 
 namespace Controllers;
 
@@ -19,111 +20,71 @@ public class AuthController : ControllerBase
         _jwtService = new JwtService(configuration);
     }
 
-    // Register endpoint
     [HttpPost("register")]
-    public async Task<IActionResult> Register([FromBody] Users user)
+    public async Task<IActionResult> Register([FromBody] UserRegisterDto user)
     {
-        // Validation block
+        if (user.email == null || user.email.Trim() == "")
+            return BadRequest("Email is required.");
+        if (user.password == null || user.password.Trim() == "")
+            return BadRequest("Password is required.");
+        if (user.name == null || user.name.Trim() == "")
+            return BadRequest("Username is required.");
 
-        // Check if the user is null or empty
-        if (string.IsNullOrEmpty(user.email))
-        {
-            return BadRequest("Email is null or Empty.");
-        }
-        else if (string.IsNullOrEmpty(user.password))
-        {
-            return BadRequest("Password is null or Empty.");
-        }
-        else if (string.IsNullOrEmpty(user.name))
-        {
-            return BadRequest("Username is null or Empty.");
-        }
-
-        // Check length of user dates
         if (user.email.Length < 5 || user.email.Length > 50)
-        {
-            return BadRequest("email must be between 5 and 50 characters.");
-        }
-        else if (user.name.Length < 3 || user.name.Length > 20)
-        {
-            return BadRequest("User name must be between 3 and 20 characters.");
-        }
-        else if (user.password.Length < 6 || user.password.Length > 20)
-        {
+            return BadRequest("Email must be between 5 and 50 characters.");
+        if (user.name.Length < 3 || user.name.Length > 20)
+            return BadRequest("Username must be between 3 and 20 characters.");
+        if (user.password.Length < 6 || user.password.Length > 20)
             return BadRequest("Password must be between 6 and 20 characters.");
-        }
 
-        // Check if the email is valid
         if (!System.Text.RegularExpressions.Regex.IsMatch(user.email, @"^[^@\s]+@[^@\s]+\.[^@\s]+$"))
-        {
             return BadRequest("Email is not valid.");
-        }
-        // Check if the password is valid
         if (!System.Text.RegularExpressions.Regex.IsMatch(user.password, @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$"))
-        {
             return BadRequest("Password must contain at least one uppercase letter, one lowercase letter, and one number.");
-        }
-        // Check if the username is valid
         if (!System.Text.RegularExpressions.Regex.IsMatch(user.name, @"^[a-zA-Z0-9]+$"))
-        {
             return BadRequest("Username must contain only letters and numbers.");
-        }
 
-        // Check if the username is already taken
         var existingUserName = await _context.Users.FirstOrDefaultAsync(u => u.name == user.name);
         if (existingUserName != null)
-        {
-            return BadRequest("A user with this username already exists.");
-        }
-        // Check if the email is already taken
+            return BadRequest("Username already exists.");
+
         var existingUserEmail = await _context.Users.FirstOrDefaultAsync(u => u.email == user.email);
         if (existingUserEmail != null)
+            return BadRequest("Email already exists.");
+
+        var newUser = new Users
         {
-            return BadRequest("A user with this email already exists.");
-        }
+            id = Guid.NewGuid(),
+            name = user.name,
+            email = user.email,
+            password = BCrypt.Net.BCrypt.HashPassword(user.password),
+            role = "user"
+        };
 
-        user.id = Guid.NewGuid();
-        user.password = BCrypt.Net.BCrypt.HashPassword(user.password); // Hash password
-
-        // Post user to database
-        await _context.Users.AddAsync(user);
+        await _context.Users.AddAsync(newUser);
         await _context.SaveChangesAsync();
 
         return Ok("User registered successfully.");
     }
 
-    // Login endpoint
     [HttpPost("login")]
-    public async Task<IActionResult> Login([FromBody] Users loginRequest)
+    public async Task<IActionResult> Login([FromBody] UserLoginDto loginRequest)
     {
-        // Validation block
-        // Check if the user is null or empty
         if (loginRequest.email == null || loginRequest.email.Trim() == "")
-        {
             return BadRequest("Email must be provided.");
-        }
-        else if (loginRequest.password == null || loginRequest.password.Trim() == "")
-        {
+        if (loginRequest.password == null || loginRequest.password.Trim() == "")
             return BadRequest("Password must be provided.");
-        }
 
-        // Have a user with these email
         var user = await _context.Users.FirstOrDefaultAsync(u => u.email == loginRequest.email);
         if (user == null)
-        {
             return Unauthorized("Incorrect email or password.");
-        }
 
-        // Check password
         bool isPasswordCorrect = BCrypt.Net.BCrypt.Verify(loginRequest.password, user.password);
         if (!isPasswordCorrect)
-        {
-            return Unauthorized("Incorrect email or password..");
-        }
+            return Unauthorized("Incorrect email or password.");
 
         string token = _jwtService.GenerateToken(user.id, user.role ?? "user");
 
-        // Return token to client
         return Ok(new
         {
             token = token,
