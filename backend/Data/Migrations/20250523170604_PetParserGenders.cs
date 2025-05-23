2198fbe90f29d987968e9322bc289d493b4154b5
Alan Arzumanjan <alanarzumanjan@gmail.com>
2025-05-23 20:46:06 +0300

    new gender migrations

---
+++ backend/Data/Migrations/20250523170604_PetParserGenders.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace main.Migrations
+{
+    /// <inheritdoc />
+    public partial class PetParserGenders : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
